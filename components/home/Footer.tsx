3b3b0d07e95fbe3177ceb27f8bce1c46d81559be
erickmshard <erick.mshard@gmail.com>
2024-09-25 22:46:14 +0800

    925

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -57,7 +57,7 @@ export default function Footer() {
     <footer className='w-full bg-[#15141A] text-white'>
       <div className='mx-auto flex min-h-[251px] max-w-pc flex-col items-center justify-between p-10 pb-5 lg:h-[180px] lg:flex-row lg:px-0 lg:pb-10'>
         <div className='flex flex-col items-center lg:items-stretch'>
-          <h1 className='text-xl font-bold lg:h-8 lg:text-[32px]'>{t('title')}</h1>
+          <h2 className='text-xl font-bold lg:h-8 lg:text-[32px]'>{t('title')}</h2>
           <h2 className='text-xs'>{t('subTitle')}</h2>
         </div>
         <div className='mt-5 flex flex-col items-center gap-y-5 lg:mt-0 lg:flex-row lg:items-stretch lg:gap-x-10'>
